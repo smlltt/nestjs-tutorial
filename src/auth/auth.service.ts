d0ba31547d5b9efc8fd637565c4cb3b1810b92a5
smlltt <samuel.liotta@gmail.com>
2023-04-13 18:08:55 +0200

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      } else if (error.code === 'P2025') {
+        throw new ForbiddenException('Foreign key constraint violation');
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  login() {
+    return 'I am logged in';
+  }
+}
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @Length(6, 20)
+  password: string;
+}
