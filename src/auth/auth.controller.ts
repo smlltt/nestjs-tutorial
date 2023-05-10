cec8bf33fbaf4e18a575752d4bec3a1c462e5664
smlltt <samuel.liotta@gmail.com>
2023-05-10 18:40:50 +0200

    custom param decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
+  @HttpCode(HttpStatus.OK) //post requests usually return 201 (status CREATED), but login does not create any resource, so I'm overriding the default status with 200
   @Post('login')
   login(@Body() dto: AuthDto) {
     return this.authService.login(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    // just proof of concept -> you can use the decorator to get a specific key of the User object
+    console.log({ email });
+    return user;
   }
 }
